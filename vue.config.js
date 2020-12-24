eb8fb0411faff10075098df03767cf50192502dd
Daxmosphere <gulongyun@imydao.com>
2020-12-24 11:02:45 +0800

    chore: vue.config.js配置添加

---
+++ vue.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: true,
+  // 关闭生产环境source-map加速生产构建和减少包大小
+  productionSourceMap: false,
+  devServer: {
+    port: 8888,
+    overlay: {
+      warning: false,
+      errors: false
+    }
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@assets/style/base.scss";`
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+        '@mixins': resolve('src/mixins'),
+        '@assets': resolve('src/assets'),
+        '@components': resolve('src/components'),
+        '@views': resolve('src/views')
+      }
+    },
+    devtool: 'cheap-module-eval-source-map',
+    output: {
+      filename: `[name].js`,
+      chunkFilename: `[name].js`
+    }
+  },
+  chainWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      if (process.env.npm_config_report) {
+        config
+          .plugin('webpack-bundle-analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+          .end()
+        config.plugins.delete('prefetch')
+      }
+    }
+  }
+}
